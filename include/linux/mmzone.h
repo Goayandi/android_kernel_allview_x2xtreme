56bfdadff0b5f4c531b6f7d535701a909c1213e8
bemolxd <kalkobb99@gmail.com>
2016-10-14 14:02:22 +0200

    Fix
    Fixed files:
     drivers/misc/Kconfig
     drivers/misc/Makefile
     include/linux/mmzone.h
     mm/vmstat.c
    
    Change-Id: Ia2294a35df2e946e65bf58b6c985c37b5f2ae616

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -156,6 +156,10 @@ enum zone_stat_item {
 #endif
 	NR_ANON_TRANSPARENT_HUGEPAGES,
 	NR_FREE_CMA_PAGES,
+#endif
+#ifdef CONFIG_UKSM
+	NR_UKSM_ZERO_PAGES,
+#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 /*
--- mm/vmstat.c
+++ mm/vmstat.c
@@ -745,6 +745,10 @@ const char * const vmstat_text[] = {
 #endif
 	"nr_anon_transparent_hugepages",
 	"nr_free_cma",
+#endif
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
